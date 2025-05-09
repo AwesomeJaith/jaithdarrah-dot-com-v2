b713085896bbc7b36b903c85e0e337059abdab78
Jaith Darrah <generic.jaithd@gmail.com>
2025-05-09 01:28:23 -0500

    Update header with a new fix for word in WordRotate component getting cut off

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   return (
     <div className=" mt-24 font-mono">
       <div className="flex flex-col gap-4 max-w-4xl mx-auto w-full px-4 sm:px-6 lg:px-8 pt-2">
-        <Header className="mb-2" />
+        <Header className="mb-4" />
         <Biography className="mb-8" />
         <WorkExperiences className="mb-8" />
         <Projects className="mb-8" />
--- src/components/jaith-darrah-dot-com-v2/header.tsx
+++ src/components/jaith-darrah-dot-com-v2/header.tsx
@@ -5,16 +5,14 @@ import { headerSentences } from "@/data/workModeData";
 const Header = ({ ...props }: React.ComponentProps<"header">) => {
   return (
     <header {...props}>
-      <div className="flex flex-col gap-4">
-        <div className="flex flex-col">
-          <h1 className="text-4xl font-bold text-gray-700">Jaith Darrah</h1>
-          <div className="h-16 sm:h-32">
-            <WordRotate
-              className="text-2xl text-gray-700"
-              duration={3500}
-              words={headerSentences}
-            />
-          </div>
+      <div className="flex flex-col gap-2">
+        <h1 className="text-4xl font-bold text-gray-700">Jaith Darrah</h1>
+        <div className="h-24 md:h-12 lg:h-8">
+          <WordRotate
+            className="text-2xl text-gray-700"
+            duration={3500}
+            words={headerSentences}
+          />
         </div>
       </div>
     </header>
