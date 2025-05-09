d32f1d3a634fd665181ab929547dc9737d7c6c6c
Jaith Darrah <generic.jaithd@gmail.com>
2025-05-08 23:17:25 -0500

    Update tooltip styling to match the website theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import WorkExperiences from "@/components/jaith-darrah-dot-com-v2/work-experienc
 
 export default function Home() {
   return (
-    <div className="mt-24 font-mono">
+    <div className=" mt-24 font-mono">
       <div className="flex flex-col gap-4 max-w-4xl mx-auto w-full px-4 sm:px-6 lg:px-8 pt-2">
         <Header className="mb-2" />
         <Biography className="mb-8" />
